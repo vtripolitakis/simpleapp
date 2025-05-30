bc22ebddf5bb277ae72dfc1b1c251d4f4cf9b280
anon <anon@anon.com>
2025-05-30 13:57:11 +0300

    [CHG] minor change

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 sudo -u postgres psql -f ./company_database.sql
-sudo -u postgres /usr/local/bin/postgres &
+sudo -u postgres /usr/local/bin/postgrest &
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+
 cd src
 npm install
-npm run dev &
+npm run build
+sudo cp -R dist/* /var/www/html/
+
